a725a657220520d025242272cc94f9773a0cd66f
SierraGolf84 <GaBabic@aol.com>
2021-04-05 11:03:42 +0200

    3rd update

--- app/src/main/java/com/example/pizzaapp/MenueActivity.java
+++ app/src/main/java/com/example/pizzaapp/MenueActivity.java
@@ -2,7 +2,9 @@ package com.example.pizzaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenueActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MenueActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menue);
     }
+
+    public void menue(View view){
+        Intent intent = new Intent(this, MenueActivity.class);
+        startActivity(intent);
+    }
+
+    public void bestellung(View view){
+        Intent intent = new Intent(this, MenueAuswahlActivity.class);
+        startActivity(intent);
+    }
+
 }
