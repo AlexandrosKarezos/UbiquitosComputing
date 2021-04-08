1983c78ace4b0ddf8e708b696b401f3393c1a8ad
Philipp_Hensen <philipp.hensen@gmail.com>
2021-04-08 12:05:53 +0200

    dynamisches Menue erstellen

--- app/src/main/java/com/example/pizzaapp/MenueActivity.java
+++ app/src/main/java/com/example/pizzaapp/MenueActivity.java
@@ -1,17 +1,53 @@
 package com.example.pizzaapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MenueActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menue);
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.menu_linear_layout);
+
+        TextView margerithaTv = new TextView(this);
+        margerithaTv.setText(getResources().getString(R.string.menu_margeritha));
+        margerithaTv.setId(margerithaTv.generateViewId());
+        margerithaTv.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        margerithaTv.setClickable(true);
+        margerithaTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menuAuswahl(view);
+            }
+        });
+
+        TextView salamiTv = new TextView(this);
+        salamiTv.setText(getResources().getString(R.string.menu_salami));
+        salamiTv.setId(salamiTv.generateViewId());
+        salamiTv.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        salamiTv.setClickable(true);
+        salamiTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menuAuswahl(view);
+            }
+        });
+
+        linearLayout.addView(margerithaTv);
+        linearLayout.addView(salamiTv);
     }
 
     public void menue(View view){
@@ -34,17 +70,7 @@ public class MenueActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void margeritha(View view){
-        Intent intent = new Intent(this, MenueAuswahlActivity.class);
-        startActivity(intent);
-    }
-
-    public void prosciutto(View view){
-        Intent intent = new Intent(this, MenueAuswahlActivity.class);
-        startActivity(intent);
-    }
-
-    public void salami(View view){
+    public void menuAuswahl(View view){
         Intent intent = new Intent(this, MenueAuswahlActivity.class);
         startActivity(intent);
     }
